9f29431394f02f3ac059b1e22505d3a3d22f4e41
Vinicius Faleiros <vinicius.faleiros@hotmail.com>
2025-07-21 21:46:05 -0300

    implement SSG on pages and finish project

--- src/hooks/use-share/use-share.tsx
+++ src/hooks/use-share/use-share.tsx
@@ -1,4 +1,4 @@
-import { Link2 } from 'lucide-react'
+import { Link } from 'lucide-react'
 import { useCallback, useMemo } from 'react'
 import { useClipboard } from '../use-clipboard'
 import {
@@ -67,7 +67,7 @@ export const useShare = ({
       {
         provider: 'clipboard',
         name: isCopied ? 'Link copiado!' : 'Copiar link',
-        icon: <Link2 className="h-4 w-4" />,
+        icon: <Link className="h-4 w-4" />,
         action: () => share('clipboard'),
       },
     ],
--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,24 +1,28 @@
 import { Search } from '@/components/search'
-import { allPosts } from 'contentlayer/generated'
+import { Post } from 'contentlayer/generated'
 import { Inbox } from 'lucide-react'
 import { useRouter } from 'next/router'
 import { PostCard } from './components/post-card'
 import { PostGridCard } from './components/post-card/post-grid-card'
 
-export default function BlogList() {
+export type BlogListProps = {
+  posts: Post[]
+}
+
+export default function BlogList({ posts }: BlogListProps) {
   const router = useRouter()
   const query = router.query.q as string
   const pageTitle = query
     ? `Resultados de busca para "${query}"`
     : 'Dicas e estratégias para impulsionar seu negócio'
 
-  const posts = query
-    ? allPosts.filter((post) =>
+  const postsList = query
+    ? posts.filter((post) =>
         post.title.toLowerCase()?.includes(query.toLowerCase()),
       )
-    : allPosts
+    : posts
 
-  const hasPosts = posts.length > 0
+  const hasPosts = postsList.length > 0
 
   return (
     <div className="flex flex-col py-24 flex-grow h-full">
@@ -43,7 +47,7 @@ export default function BlogList() {
       {/* Listagem de posts */}
       {hasPosts && (
         <PostGridCard>
-          {posts.map((post) => (
+          {postsList.map((post) => (
             <PostCard
               key={post._id}
               title={post.title}
