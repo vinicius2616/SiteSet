3d605c604459f48f945eb506d9ef015e1d6dfde2
Vinicius Faleiros <vinicius.faleiros@hotmail.com>
2025-10-24 17:01:05 -0300

    add seo in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
 import LandingPage from '@/templates/landing-page/landing-page'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Site.Set',
+  description: 'Venda seus produtos como afiliado em um único lugar',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Venda seus produtos como afiliado em um único lugar',
+    url: 'https://site-set-iota.vercel.app/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt_BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://site-set-iota.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      },
+    ],
+  },
+}
 
 export default function HomePage() {
   return <LandingPage />
-
-  // return <ErrorBoundary />
 }
