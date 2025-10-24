0e0cf6aa581a95495ce7b35bbe80ca0798b40b9b
Vinicius Faleiros <vinicius.faleiros@hotmail.com>
2025-10-24 17:07:03 -0300

    add seo in blog page list

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,28 @@
 import BlogList from '@/templates/blog/blog-list'
 import { allPosts } from 'contentlayer/generated'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Blog',
+  description: 'Dicas e estratégias para impulsionar o negócio',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Dicas e estratégias para impulsionar o negócio',
+    url: 'https://site-set-iota.vercel.app/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt_BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://site-set-iota.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      },
+    ],
+  },
+}
 
 export default function BlogListPage() {
   const sortedPosts = allPosts.sort(
